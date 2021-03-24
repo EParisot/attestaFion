f4ae226fbaac9b63ec8cf22e001075645e829d2c
eparisot <eparisot@student.42.fr>
2021-03-24 02:04:02 +0100

    --allow-unauthenticated

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 gcloud config set project attestafion
 git pull
 gcloud builds submit --tag gcr.io/attestafion/attestafion
-gcloud run deploy --image gcr.io/attestafion/attestafion --platform managed
+gcloud run deploy --image gcr.io/attestafion/attestafion --platform managed --allow-unauthenticated
