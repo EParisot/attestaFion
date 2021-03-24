99e30b8e575e0a9dffcc04a6f923240bec902030
eparisot <eparisot@student.42.fr>
2021-03-24 02:54:53 +0100

    update

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcloud config set project attestafion
-git pull
-gcloud builds submit --tag gcr.io/attestafion/attestafion
-gcloud run deploy --image gcr.io/attestafion/attestafion --platform managed
